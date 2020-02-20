fb8ff048698ed24b3f4e2b36e6f742ce6be604d7
tokky <otokita@pepabo.com>
2020-02-20 11:52:45 +0900

    rack/lib/lobsterをマウント

--- lobster/config/routes.rb
+++ lobster/config/routes.rb
@@ -1,3 +1,6 @@
+require 'rack/lobster'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount Rack::Lobster.new, at: '/hoge'
 end
